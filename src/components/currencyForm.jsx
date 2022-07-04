34f89c0509a6bb8fdba95240829a6428b408ce3e
Jean Carlos Villarroel <jeancjvp@gmail.com>
2022-07-04 17:31:41 -0500

    heroku commit

---
+++ src/components/currencyForm.jsx
@@ -0,0 +1,55 @@
+// Main Imports
+import { useState } from 'react';
+
+// Currencies
+import { currencies } from '../helpers/supportedSymbols.js';
+
+// Components
+import Input from './input';
+import OptionsInput from './optionsInput';
+
+function CurrencyForm({ handleSubmit, result = '' }) {
+
+	const [currencyFrom, setCurrencyFrom] = useState();
+	const [currencyTo, setCurrencyTo] = useState();
+
+	// Setting Country names onChange
+	function onChangeOption(name) {
+		if (name === 'currencyFrom') setCurrencyFrom(document.querySelector('select[name="currencyFrom"]').value);
+		if (name === 'currencyTo') setCurrencyTo(document.querySelector('select[name="currencyTo"]').value);
+	}
+
+	return (
+		<>
+			<form onSubmit={ handleSubmit }>
+				<div className='form-group mb-4'>
+					<h6 className='subtitle text-white'>From: <span className='country'>{ currencyFrom ? currencies[currencyFrom]['currencyName'] : null }</span></h6>
+					<div className='justify-content-center row'>
+						<div className='col-8 p-0'>
+							<Input type={'number'} name={'amountFrom'} placeholder={'Amount'}/>
+						</div>
+						<div className='col p-0'>
+							<OptionsInput name={'currencyFrom'} options={ Object.keys(currencies) } onChangeOption={ onChangeOption }/>
+						</div>
+					</div>
+				</div>
+
+				<div className='form-group my-4'>
+					<h6 className='subtitle text-white'>To: <span className='country'>{ currencyTo ? currencies[currencyTo]['currencyName'] : null }</span></h6>
+					<div className='justify-content-center row'>
+						<div className='col-8 p-0'>
+							<div className='output p-2 bg-white'>{ !result ? 'Calculating...' : result }</div>
+						</div>
+						<div className='col p-0'>
+							<OptionsInput name={'currencyTo'} options={ Object.keys(currencies) } onChangeOption={ onChangeOption }/>
+						</div>
+					</div>
+				</div>
+
+				<button type='submit' className='btn btn-primary px-4 py-2 mt-4'>Calculate</button>
+			</form>
+		</>
+	);
+}
+
+export default CurrencyForm;
