e1edae3727db640c0fa50c4258ccc85c59b4490a
Jean Carlos Villarroel <jeancjvp@gmail.com>
2022-07-04 17:31:05 -0500

    heroku commit

--- public/src/components/input.jsx
+++
@@ -1,9 +0,0 @@
-function Input({ type, name, placeholder = '' }) {
-	return (
-		<>
-			<input type={ type } className='p-2' name={ name } placeholder={ placeholder } step={ type === 'number' ? '0.01' : null }/>
-		</>
-	);
-}
-
-export default Input;
